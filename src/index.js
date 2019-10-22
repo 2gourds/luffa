b4de49c051578cc57ba9249bb02d616684621b44
2gourds <arben.garcia@gmail.com>
2019-10-22 21:04:41 +0900

    Added tutorial files and modified code to use JSX notation.

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+  render() {
+    return React.createElement('button', {className: 'square'}, this.props.value);
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return React.createElement('div', null, 
+      React.createElement('div', {className: 'status'}, status),
+      React.createElement('div', {className: 'board-row'}, 
+        this.renderSquare(0),
+        this.renderSquare(1),
+        this.renderSquare(2)
+      ),
+      React.createElement('div', {className: 'board-row'}, 
+        this.renderSquare(3),
+        this.renderSquare(4),
+        this.renderSquare(5)
+      ),
+      React.createElement('div', {className: 'board-row'}, 
+        this.renderSquare(6),
+        this.renderSquare(7),
+        this.renderSquare(8)
+      ),
+    );    
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
