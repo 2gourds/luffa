caef18ef97b7bee138bf0783695f51caf3c09a57
2gourds <arben.garcia@gmail.com>
2019-10-23 01:16:56 +0900

    Change 3x3 grid to variable length grid (9x9).

--- src/index.js
+++ src/index.js
@@ -16,23 +16,22 @@ class Board extends React.Component {
   render() {
     const status = 'Next player: X';
 
+    var gridCount = 10;
+    var row = [];
+    for (var i = 0; i < gridCount; i++) {
+      row.push(this.renderSquare(i));
+    }
     return React.createElement('div', null, 
       React.createElement('div', {className: 'status'}, status),
-      React.createElement('div', {className: 'board-row'}, 
-        this.renderSquare(0),
-        this.renderSquare(1),
-        this.renderSquare(2)
-      ),
-      React.createElement('div', {className: 'board-row'}, 
-        this.renderSquare(3),
-        this.renderSquare(4),
-        this.renderSquare(5)
-      ),
-      React.createElement('div', {className: 'board-row'}, 
-        this.renderSquare(6),
-        this.renderSquare(7),
-        this.renderSquare(8)
-      ),
+      React.createElement('div', {className: 'board-row'}, row),
+      React.createElement('div', {className: 'board-row'}, row),
+      React.createElement('div', {className: 'board-row'}, row),
+      React.createElement('div', {className: 'board-row'}, row),
+      React.createElement('div', {className: 'board-row'}, row),
+      React.createElement('div', {className: 'board-row'}, row),
+      React.createElement('div', {className: 'board-row'}, row),
+      React.createElement('div', {className: 'board-row'}, row),
+      React.createElement('div', {className: 'board-row'}, row),
     );    
   }
 }
