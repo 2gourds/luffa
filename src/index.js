c60e3ca39b4f5165125a9f59bf17ceeb6d86737e
2gourds <arben.garcia@gmail.com>
2019-10-23 01:56:03 +0900

    Added on-click interaction into the grid elements.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
-    return React.createElement('button', {className: 'square'}, this.props.value);
+    var props = {
+        className: 'square',
+        onClick: () => this.setState({value: 'X'})
+    }
+    return React.createElement('button', props, this.state.value);
   }
 }
 
