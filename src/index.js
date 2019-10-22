183cda216770f9b0a0467ab860c415dd290dc80f
2gourds <arben.garcia@gmail.com>
2019-10-23 01:28:19 +0900

    Programmatically generate the grids of the board; change the grid count to 8x8.

--- src/index.js
+++ src/index.js
@@ -15,25 +15,24 @@ class Board extends React.Component {
 
   render() {
     const status = 'Next player: X';
+    var gridCount = 8;
 
-    var gridCount = 10;
+    // Create row of specific number of grids (8 as default).
     var row = [];
     for (var i = 0; i < gridCount; i++) {
       row.push(this.renderSquare(i));
     }
+
+    // Create specific number of div elements for rows.
+    var board = [];
+    for (var i = 0; i < gridCount; i++) {
+      board.push(React.createElement('div', {className: 'board-row'}, row));
+    }
+    
     return React.createElement('div', null, 
-      React.createElement('div', {className: 'status'}, status),
-      React.createElement('div', {className: 'board-row'}, row),
-      React.createElement('div', {className: 'board-row'}, row),
-      React.createElement('div', {className: 'board-row'}, row),
-      React.createElement('div', {className: 'board-row'}, row),
-      React.createElement('div', {className: 'board-row'}, row),
-      React.createElement('div', {className: 'board-row'}, row),
-      React.createElement('div', {className: 'board-row'}, row),
-      React.createElement('div', {className: 'board-row'}, row),
-      React.createElement('div', {className: 'board-row'}, row),
-    );    
+      React.createElement('div', {className: 'status'}, status), board);
   }
+
 }
 
 class Game extends React.Component {
